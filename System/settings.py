85208454e13793c4a457e72b39cabfb3ba5c54f6
Toby <tobybecerra04@gmail.com>
2025-03-09 20:52:12 +0545

    Build: change the System carpet

--- System/settings.py
+++ System/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tks=@192+4)7jud#mivruaeo-dj25(1k4d8pgxp2im+6p2*a5s'
+SECRET_KEY = 'django-insecure-*h3gch%huj%7x2bkf_gutbaggt=su^#rnisy3a*e0m$-xe17t_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
