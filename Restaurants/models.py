9794da6a1a9690f4e212f5a183b528bb981da8a2
Toby <tobybecerra04@gmail.com>
2025-04-22 12:27:59 +0545

    Feature : add the restaurant form and create the admin user

--- Restaurants/models.py
+++ Restaurants/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    address = models.CharField(max_length=255)
+    phoneNumber = models.CharField(max_length=15)
+    website = models.URLField(blank=True, null=True)
+    rating = models.FloatField(default=0.0)
+    cuisineType = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
