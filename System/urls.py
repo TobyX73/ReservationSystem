9b3b9a629e77dffd47c1e225485b37dceaf66d88
Toby <tobybecerra04@gmail.com>
2025-04-28 11:15:33 -0300

    Build : add the client carpet and React + ViteJS + TS for front end

--- System/urls.py
+++ System/urls.py
@@ -21,5 +21,5 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Rest/', include('Restaurants.urls')),
-    path("docs/", include_docs_urls (title="Restarunt API")),
+    path("docs/", include_docs_urls (title="Restaurant API")),
 ]
