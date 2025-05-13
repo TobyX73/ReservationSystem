d529b641c41c36f0592262ba90525d6d77862a60
Toby <tobybecerra04@gmail.com>
2025-05-13 11:57:19 -0300

    Fix: Change the name RestaurantAPI to Restaurants, fixed a bug with the name in app, settings and URL files

--- Restaurants/apps.py
+++ Restaurants/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class RestaurantsConfig(AppConfig):
+class RestaurantApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Restaurants'
--- client/src/pages/RestaurantPage.tsx
+++ client/src/pages/RestaurantPage.tsx
@@ -1,6 +1,11 @@
 import RestaurantList from '../components/RestaurantList';
+import { useEffect } from 'react'; 
 
 export default function RestaurantPage() {
+    useEffect(() => {
+        console.log("Restaurant Page");
+    }, []);
+
     return (
         <div>
             <RestaurantList />
